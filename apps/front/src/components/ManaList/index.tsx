a448c74a819fbbfa1a6b562342233dbc09151351
Lucas <lucas.daval@edu.devinci.fr>
2023-02-21 16:40:12 +0100

    WIP - manaList

--- apps/front/src/components/ManaList/index.tsx
+++ apps/front/src/components/ManaList/index.tsx
@@ -1,23 +1,46 @@
 import { ManaItem } from "../ManaItem";
-import styles from "./style.module.css";
-import { ManaAvailable } from "./types";
+import * as styles from "./style.module.css";
+
+export const ManaList = () => {
 
 
-export const ManaList = (props:ManaAvailable) => {
-
-    const manas = []
-    
-
-    for(let i = 0; i < 10; i++){
-        manas.push({disabled : true})
-    }
-
-    for(let i = 0; i <  props.manaToUse; i++){
-        manas[i].disabled = false
-    }
+    const manas = [
+        {
+            disabled : true,
+            key: 0
+        },
+        {
+            disabled : true,
+            key: 1
+        },
+        {
+            disabled : true,
+            key: 2
+        },
+        {
+            disabled : true,
+            key: 3
+        },
+        {
+            disabled : true,
+            key: 4
+        },
+        {
+            disabled : false,
+            key: 5
+        },
+        {
+            disabled : false,
+            key: 6
+        },
+        {
+            disabled : false,
+            key: 7
+        }
+    ]
 
     return(
-        <div className={styles.manaRow}>
+        <div className={styles.row}>
             {
                 manas.map((mana) => (
                     <ManaItem isUsed={mana.disabled}/>
