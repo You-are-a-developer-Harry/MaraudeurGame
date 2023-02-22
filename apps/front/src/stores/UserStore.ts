569f5f6c110a180cc201ebb078051085604f87eb
Armand Dorard <armand.dorard@commoprices.com>
2023-02-22 12:16:03 +0100

    Login done, need to put the user in the store

--- apps/front/src/stores/UserStore.ts
+++ apps/front/src/stores/UserStore.ts
@@ -9,10 +9,34 @@ interface UserStore {
 export const useUserStore = create<UserStore>((set) => ({
   user: {
     id: Math.round(Math.random() * 1000),
-    username: 'Test',
-    house: Object.values(HogwartHouse)[Math.round(Math.random() * 3)],
+    name: 'Test',
+    points: 0,
+    house_id: 1,
+    house: {
+      id : 1,
+      name: Object.values(HogwartHouse)[Math.round(Math.random() * 3)],
+      points: 0,
+    }
   },
   setUser(user) {
     set({ user })
   },
 }))
+
+
+/*
+{
+    "user": {
+        "id": 6,
+        "name": "test1",
+        "points": 0,
+        "house_id": 1,
+        "house": {
+            "id": 1,
+            "points": 0,
+            "name": "Serpentard"
+        }
+    },
+    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjYsImlhdCI6MTY3NzA2MjcyOSwiZXhwIjoxNjc3MDk4NzI5fQ.hHLQhRduCLGnJ59Vtu1MyycyJsnhxqjklqC93gnevzo"
+}
+ */
