bae7e8228eef75b54d70c01dede28da4fc3cff75
robinlrx <robin@monkey-lab.io>
2023-02-23 11:06:30 +0100

    add image depeding on house

--- apps/front/src/components/Leaderboard/index.tsx
+++ apps/front/src/components/Leaderboard/index.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
 import styles from "./style.module.css";
+import { HogwartHouse } from "types";
 
 const Leaderboard = ({winPlayer}: {winPlayer: any}) => {
 	console.log('leaderbord win', winPlayer)
+	// set image and color depending on winner player
+	let house = winPlayer.house.name
+	let image
+	let color
+	switch (house) {
+		case HogwartHouse.GRYFFONDOR:
+			image = 'image/griff.png'
+			color = '#b33429'
+			break
+		case HogwartHouse.SLYTHERIN:
+			image = 'image/serp.png'
+			color = '#386a2c'
+			break
+		case HogwartHouse.HUFFLEPUFF:
+			image = 'image/pouf.png'
+			color = '#dfbe45'
+			break
+		case HogwartHouse.RAVENCLAW:
+			image = 'image/serd.png'
+			color = '#408fb1'
+			break
+		default: HogwartHouse.GRYFFONDOR
+			image = 'image/grif.png'
+			color = '#b33429'
+			break
+	}
+	
 	return (
 		<div className={styles.container}>
-			<h1 className={styles.title}>The winner is <span>{winPlayer.name}</span> from {winPlayer.house.name} !</h1>
+			<h1 className={styles.title}>The winner is <span style={{color: color}}>{winPlayer.name}</span> from <span style={{color: color}}>{winPlayer.house.name}</span> !</h1>
+			<img src={image} alt="" className={styles.house}/>
 		</div>
 	);
 };
