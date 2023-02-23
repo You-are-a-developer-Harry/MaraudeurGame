753823f25fdedfcbde3fa637399ab0e127ade87f
Maxime Perraud <contact@maxime-p.dev>
2023-02-23 13:15:48 +0100

    feat: finished victory function

--- apps/back/src/utils/victoryGame.ts
+++ apps/back/src/utils/victoryGame.ts
@@ -1,11 +1,17 @@
 import { Socket } from "socket.io";
 import { getCurrentRoom } from "./socketHelpers";
-import { boards } from "./data";
+import { boards, stateMachines } from "./data";
 import { Player } from "../types";
+import axios from "axios";
 
 export const victoryGame = (player: Player, socket: Socket) => {
   const currentRoom = getCurrentRoom(socket)
   const gameId = boards
     .get(currentRoom)!.gameId
 
+  axios.post('https://hp-api-iim.azurewebsites.net/matches/end', {
+    gameId: gameId, userId: player.id
+  }).then(data => {
+    stateMachines.get(currentRoom)!.send('STOP')
+  })
 }
