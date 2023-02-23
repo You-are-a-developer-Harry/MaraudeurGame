dc7ecf396abd01fc4397909fa1fdccf79a70ff84
Lucas <lucas.daval@edu.devinci.fr>
2023-02-23 11:37:24 +0100

    FIX - images's houses

--- apps/front/src/utils/getHouseImage.ts
+++ apps/front/src/utils/getHouseImage.ts
@@ -1,8 +1,8 @@
 import { HogwartHouse } from "../types"
-import GrifImg from '../assets/images/houses/grif.png'
-import SlythImg from '../assets/images/houses/serp.png'
-import HuffleImg from '../assets/images/houses/pouf.png'
-import RavenImg from '../assets/images/houses/serd.png'
+import GrifImg from '../assets/images/houses/lion.png'
+import SlythImg from '../assets/images/houses/snake.png'
+import HuffleImg from '../assets/images/houses/badger.png'
+import RavenImg from '../assets/images/houses/eagle.png'
 
 export function getHouseImage(house: string) {
   let image = ""
