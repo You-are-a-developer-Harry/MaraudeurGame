2631332c92088197d10172fccd4f0af2d89e8bbc
robinlrx <robin@monkey-lab.io>
2023-02-23 10:28:56 +0100

    show winner on leaderboard

--- apps/front/src/components/BoardGame/index.tsx
+++ apps/front/src/components/BoardGame/index.tsx
@@ -7,6 +7,7 @@ import { HPButton } from "../HPButton";
 import Dice from "../Dice";
 import { useState } from "react";
 import { SpellAnimation } from "@components/SpellAnimation";
+import Leaderboard from "@components/Leaderboard";
 
 import spell1 from "@assets/images/spells/spell1.png";
 import spell2 from "@assets/images/spells/spell2.png";
@@ -101,7 +102,12 @@ const inventory = [
   },
 ]
 
-export const BoardGame = () => {
+type BoardGameProps = {
+	showLeaderboard: boolean;
+	winner: string;
+}
+
+export const BoardGame = ({ showLeaderboard, winner }: BoardGameProps ) => {
   const [diceValue, setDiceValue] = useState(1)
   const [displayDice, setDisplayDice] = useState(false)
   const [displayAnimation, setDisplayAnimation] = useState(false);
@@ -141,6 +147,8 @@ export const BoardGame = () => {
     }, 5000)
   }
 
+  console.log('winner:', winner)
+  console.log('showLeaderboard:', showLeaderboard)
 
   return (
     <div className={style.boardGameGrid}>
@@ -191,7 +199,11 @@ export const BoardGame = () => {
 
       {displayDice && <Dice diceValue={diceValue} />}
 
-      {displayAnimation && <SpellAnimation spell={selectedSpell} />}
+      {
+        displayAnimation &&
+        <SpellAnimation spell={selectedSpell}/>
+      }
+	  {showLeaderboard && <Leaderboard winPlayer={winner} />}
     </div>
   )
 }
