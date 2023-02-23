0c3af3063ee40a78a0b15501dc50dd244faa3ab2
Armand Dorard <armand.dorard@commoprices.com>
2023-02-23 11:47:03 +0100

    make-the-fetch-for-startGame

--- apps/back/src/utils/statGame.ts
+++ apps/back/src/utils/statGame.ts
@@ -7,4 +7,12 @@ export const startGame = (socket: Socket) => {
   const currentRoom = getCurrentRoom(socket)
   logger.info('Game start in room %s', currentRoom)
   stateMachines.get(currentRoom)!.send('START')
+
+  // call the api to tell them the game started
+  const token = '' // user.token
+  fetch('https://hp-api-iim.azurewebsites.net/matches/start', {
+      method: 'POST',
+      body: JSON.stringify({ game: "MaraudeurGame", userIds: ["userID1","userID2","userID3","userID4"], type: "1v1"}),
+      headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`},
+    })
 }
